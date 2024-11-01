35dabfd6dcc33e264844d6ce07f62001d404d181
sachinsebastian1212 <43450034+sachinsebastian1212@users.noreply.github.com>
2024-11-01 13:56:58 +0530

    added image picker for signup

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/widgets/user_image_picker.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -65,6 +66,7 @@ class _AuthScreenState extends State<AuthScreen> {
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: [
+                            if (!_isLogin) const UserImagePicker(),
                             TextFormField(
                               decoration: const InputDecoration(
                                   labelText: 'Email Address'),
