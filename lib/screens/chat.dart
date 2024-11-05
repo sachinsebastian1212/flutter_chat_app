9bd6151e6a9c4f2d18928a59ac58d5352acf622b
s <sachin@sachins-MacBook-Pro.local>
2024-11-05 15:57:18 +0530

    added widgets in chatscreen

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/widgets/chat_messages.dart';
+import 'package:flutter_chat_app/widgets/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -20,7 +22,12 @@ class ChatScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: const Center(child: Text('logged in!')),
+      body: const Column(
+        children: [
+          Expanded(child: ChatMessages()),
+          NewMessage()
+        ],
+      )
     );
   }
 }
---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _messagingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messagingController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() {
+    final enteredMessage = _messagingController.text;
+
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+
+    //send to firebase
+    _messagingController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
+      child: Row(
+        children: [
+          Expanded(
+              child: TextField(
+            controller: _messagingController,
+            textCapitalization: TextCapitalization.sentences,
+            autocorrect: true,
+            enableSuggestions: true,
+            decoration: const InputDecoration(labelText: 'Send a message...'),
+          )),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
+      ),
+    );
+  }
+}
